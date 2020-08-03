89c5e778e5784ecee6810cd2e56e15a09f0957ba
stevewquezada <stevewquezada@gmail.com>
2020-08-03 15:59:40 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 const prod = process.env.NODE_ENV === 'production'
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-const withCss = require('@zeit/next-css');
-const withPurgeCss = require('next-purgecss');
 
-module.exports = withPlugins([
-    [optimizedImages, {
+
+module.exports = {
     env: {
         ProjectUrl: prod ? 'https://stm.raxo.dev/' : 'http://stm.test/',
     },
     target: 'serverless',
-    }],  
-]);
+}
 
-module.exports = withCss(withPurgeCss())
