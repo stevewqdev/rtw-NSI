2a5485ead300ddd91a44009aa45ce1fbb7e1d881
stevewquezada <stevewquezada@gmail.com>
2020-08-20 08:20:06 -0400

    fix for donation page

--- pages/donate.js
+++ pages/donate.js
@@ -35,7 +35,6 @@ export default class MakeDonation extends Component {
                     donation: true,
                 }); 
 
-                location.href = "#donation__message";
             }
         })
        
