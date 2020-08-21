d7acabdfff8d2fd09594d8d84e58627ae627c30a
stevewquezada <stevewquezada@gmail.com>
2020-08-21 14:14:18 -0400

    added production API link

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,7 @@ var currentPage = 0,
     apiCallData;
     
 // We set the API link to call 
-var apiLink = 'https://raxo.dev/stm/api/'; 
+var apiLink = 'https://raxo.dev/stm/api/prod.php'; 
 
 // We make the API call to pull the data
 var apiCall = $.getJSON(`${apiLink}`, function(data) {
