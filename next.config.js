54db628063a5f414205c4e91904fbb5b7e68dc6d
steveraxo <steve@raxo.tv>
2021-02-05 16:30:38 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,1 +1,7 @@
 const prod = process.env.NODE_ENV === 'production'
+
+module.exports = {
+    // Target must be experimental-serverless-trace
+    // Your build time will be longer with this option
+    target: "experimental-serverless-trace",
+};
