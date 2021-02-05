f31112182318f48c266ac5e3405e42f259eea69c
steveraxo <steve@raxo.tv>
2021-02-05 16:22:50 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
 const prod = process.env.NODE_ENV === 'production'
-
-
-module.exports = {
-    env: {
-        ProjectUrl: prod ? 'https://cms.servethemoment.org/' : 'https://cms.servethemoment.org/',
-    },
-    target: 'serverless',
-}
-
