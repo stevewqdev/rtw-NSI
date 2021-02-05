3979b7a2f44698bbca85e24761d3cb218d259d67
steveraxo <steve@raxo.tv>
2021-02-05 16:37:28 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 const prod = process.env.NODE_ENV === 'production'
 
-module.exports = {
-    // Target must be experimental-serverless-trace
-    // Your build time will be longer with this option
-    target: "serverless",
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-        require("./scripts/generate-sitemap");
-        }
 
-        return config;
+module.exports = {
+    env: {
+        ProjectUrl: prod ? 'https://cms.servethemoment.org/' : 'https://cms.servethemoment.org/',
     },
-};
+    target: "serverless",
+}
+
